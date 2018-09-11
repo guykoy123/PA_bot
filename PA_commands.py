51a1a0f4c30cc4f292f1a3ce987d9eef4d15cf5e
guykoy123 <23092757+guykoy123@users.noreply.github.com>
2018-09-11 12:01:30 +0300

    fixed reminders and weather, added driver lock

--- PA_commands.py
+++ PA_commands.py
@@ -1,7 +1,15 @@
 import time
+from weather import Weather, Unit
 
 def get_date():
     return time.strftime("%a, %d %b %Y", time.gmtime())
+
+def get_weather_forecast(location='tel aviv'):
+    weather=Weather(unit=Unit.CELSIUS)
+    location=weather.lookup_by_location(location)
+    condition=location.condition
+    message=[get_date(),condition.temp+"°C, "+condition.text]
+    return message
 
 def scream():
     message=""
